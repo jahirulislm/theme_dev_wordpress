1f365c65fa2ea55348a941d9d1c73ae2355bdb10
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-05-21 17:11:33 +0600

    learnThemedev

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer-area">
+    <section id='copyright-area'>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><?php echo get_theme_mod('try_copyright_section');?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
+
+<?php wp_footer();?>
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* My Theme Function
+*/
+
+// ALl Default theme function here
+include_once('feathureFunction/default.php');
+
+
+// Theme CSS and jQuery File calling
+include_once('feathureFunction/enqueue.php');
+
+
+//Theme Function
+include_once('feathureFunction/theme_function.php');
+
+
+// Menu Register
+include_once('feathureFunction/menu_register.php');
