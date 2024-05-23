ab34205e1b03915f9b7e2467aa976349df9aa7ec
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-05-24 00:28:56 +0600

    functions.php and theme.json added

---
+++ Custom_Functionlity(functions.php)/functions.php
@@ -0,0 +1,6 @@
+<!-- The functions.php essentially acts like a WordPress plugin, letting you add custom PHP functions, classes, interfaces, and more. It opens up the entirety of the PHP programming language to your theme.
+
+WordPress automatically loads the functions.php file 
+
+
+The only caveat to that rule is when a child theme is active. In that case, WordPress loads the child theme’s functions.php just before loading the parent theme’s functions.php.-->
