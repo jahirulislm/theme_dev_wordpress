814e1f62bf853fd6a4d754845cd8270c0eae9f06
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-05-25 14:10:21 +0600

    learn about templating

--- Custom_Functionlity(functions.php)/functions.php
+++ Custom_Functionlity(functions.php)/functions.php
@@ -1,6 +1,23 @@
-<!-- The functions.php essentially acts like a WordPress plugin, letting you add custom PHP functions, classes, interfaces, and more. It opens up the entirety of the PHP programming language to your theme.
+The functions.php essentially acts like a WordPress plugin, letting you add custom PHP functions, classes, interfaces,
+and more. It opens up the entirety of the PHP programming language to your theme.
 
-WordPress automatically loads the functions.php file 
+WordPress automatically loads the functions.php file
 
 
-The only caveat to that rule is when a child theme is active. In that case, WordPress loads the child theme’s functions.php just before loading the parent theme’s functions.php.-->
+The only caveat to that rule is when a child theme is active. In that case, WordPress loads the child theme’s
+functions.php just before loading the parent theme’s functions.php.
+
+
+add_filter( 'default_wp_template_part_areas', 'themeslug_template_part_areas' );
+
+function themeslug_template_part_areas( array $areas ) {
+$areas[] = array(
+'area' => 'loop',
+'area_tag' => 'section',
+'label' => __( 'Loop', 'themeslug' ),
+'description' => __( 'Custom description', 'themslug' ),
+'icon' => 'layout'
+);
+
+return $areas;
+}
